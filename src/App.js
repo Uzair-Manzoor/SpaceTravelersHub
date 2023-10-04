fd767e749a91d5c6776b6f6869f50283a0729921
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-10-05 01:24:34 +0500

    finalize missions task styling and functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Route, Routes } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import MyProfile from './components/myProfile';
+import Missions from './components/missions';
 import Rockets from './components/Rockets';
+import './styles/App.css';
 
 function App() {
   return (
-    <>
-      <div>
-        <Navbar />
-      </div>
-      <div className="main-container">
-        <Routes>
-          <Route path="/" element={<Rockets />} />
-        </Routes>
-      </div>
-    </>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route className="Missions" path="/react-capstone/missions" element={<Missions />} />
+        <Route path="/react-capstone/myProfile" element={<MyProfile />} />
+      </Routes>
+    </div>
   );
 }
 
