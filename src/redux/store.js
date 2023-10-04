7431af47f92218a67a22ec77241afb53aaf14a37
Adeshina Adenuga <Nuga0909@gmail.com>
2023-10-04 05:50:29 +0100

    fix linter errors

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,9 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import rocketSlice from './rocketslice';
 
 const reducer = combineReducers({
-    rockets: rocketSlice,
+  rockets: rocketSlice,
 });
 const store = configureStore({
-    reducer,
+  reducer,
 });
 export default store;
