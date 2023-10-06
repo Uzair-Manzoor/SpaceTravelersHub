df167f3d77aac5adf9d6477bfdfdb31a8267ede9
Adeshina Adenuga <Nuga0909@gmail.com>
2023-10-06 14:28:00 +0100

    fix linter errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import planetImg from '../assets/images/planet.png';
-import '../styles/App.css';
 import '../styles/rocket.css';
 
 const Navbar = () => {
