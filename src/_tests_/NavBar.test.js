4924c5f71c2d705e4f719ae112372adc0a91d581
Adeshina Adenuga <Nuga0909@gmail.com>
2023-10-06 09:09:04 +0100

    fix linter  errors

--- src/_tests_/NavBar.test.js
+++ src/_tests_/NavBar.test.js
@@ -2,44 +2,44 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 
 describe('Navbar Component', () => {
-    beforeEach(() => {
-        render(
-            <Router>
-                <Navbar />
-            </Router>
-        );
-    });
+  beforeEach(() => {
+    render(
+      <Router>
+        <Navbar />
+      </Router>,
+    );
+  });
 
-    it('renders the logo', () => {
-        const logoElement = screen.getByAltText('');
-        expect(logoElement).toBeInTheDocument();
-    });
+  it('renders the logo', () => {
+    const logoElement = screen.getByAltText('');
+    expect(logoElement).toBeInTheDocument();
+  });
 
-    it('renders the header text', () => {
-        const headerElement = screen.getByText(/Space Traveller's Hub/i);
-        expect(headerElement).toBeInTheDocument();
-    });
+  it('renders the header text', () => {
+    const headerElement = screen.getByText(/Space Traveller's Hub/i);
+    expect(headerElement).toBeInTheDocument();
+  });
 
-    it('renders navigation links', () => {
-        const rocketsLink = screen.getByText(/Rockets/i);
-        const missionsLink = screen.getByText(/Missions/i);
-        const profileLink = screen.getByText(/My Profile/i);
+  it('renders navigation links', () => {
+    const rocketsLink = screen.getByText(/Rockets/i);
+    const missionsLink = screen.getByText(/Missions/i);
+    const profileLink = screen.getByText(/My Profile/i);
 
-        expect(rocketsLink).toBeInTheDocument();
-        expect(missionsLink).toBeInTheDocument();
-        expect(profileLink).toBeInTheDocument();
-    });
+    expect(rocketsLink).toBeInTheDocument();
+    expect(missionsLink).toBeInTheDocument();
+    expect(profileLink).toBeInTheDocument();
+  });
 
-    it('has correct link paths', () => {
-        const rocketsLink = screen.getByText(/Rockets/i).closest('a');
-        const missionsLink = screen.getByText(/Missions/i).closest('a');
-        const profileLink = screen.getByText(/My Profile/i).closest('a');
+  it('has correct link paths', () => {
+    const rocketsLink = screen.getByText(/Rockets/i).closest('a');
+    const missionsLink = screen.getByText(/Missions/i).closest('a');
+    const profileLink = screen.getByText(/My Profile/i).closest('a');
 
-        expect(rocketsLink).toHaveAttribute('href', '/');
-        expect(missionsLink).toHaveAttribute('href', '/react-capstone/missions');
-        expect(profileLink).toHaveAttribute('href', '/react-capstone/myprofile');
-    });
+    expect(rocketsLink).toHaveAttribute('href', '/');
+    expect(missionsLink).toHaveAttribute('href', '/react-capstone/missions');
+    expect(profileLink).toHaveAttribute('href', '/react-capstone/myprofile');
+  });
 });
